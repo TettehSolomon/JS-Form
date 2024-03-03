81d7160ef87ea20688512d1d6e1a374435e2e25f
TettehSolomon <tettehsolomon106@gmail.com>
2024-03-03 11:02:57 +0100

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,50 @@
-//create a function
+//create a function that displays all outputs on submit
 function userInput() {
-    
-   if (document.getElementById('radioOne').checked == true) {
-    console.log('Personal');
-   } else {
-    console.log('Company');
-   }
 
+    //create a variable that receives the radioOne input
+    let radioOne = document.getElementById('radioOne').checked;
+
+    //create a variable that receives the radioTwo input
+    let radioTwo = document.getElementById('radioTwo').checked;
+
+    //Use the if & else if condition to display my output in the console
+    if (radioOne == true){
+        console.log('Personal')
+    } else if (radioTwo == true){
+        console.log('Company');
+    }
+
+    //get name input and store it in a variable
     let name = document.getElementById('name').value;
+
+    //display the output via console
     console.log(name);
 
+    //get email input and store it in a variable
     let email = document.getElementById('email').value;
+
+    //display the output via console
     console.log(email);
 
+    //get password input and store it in a variable
     let password = document.getElementById('password').value;
+
+    //display the out via console
     console.log(password);
 
-    // let male = document.getElementById('male').value;
-    // console.log(male);
+     //create a variable that receives the male input
+    let male = document.getElementById('male').checked;
 
-    // let female = document.getElementById('female').value;
-    // console.log(female);
+     //create a variable that receives the female input
+    let female = document.getElementById('female').checked;
 
+    
+    //Use the if & else if condition to display my output in the console
+    if(male == true) {
+        console.log('male');
+    } else if (female == true) {
+        console.log('female');
+    }
 }
 
 userInput();
